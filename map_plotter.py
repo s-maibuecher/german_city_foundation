38fee0e179b713f0f9c05bc5e0e8c0a9849f3d53
Stefan Maibücher <s.maibuecher@gmail.com>
2018-09-06 16:29:22 +0200

    schönere Karten

--- map_plotter.py
+++ map_plotter.py
@@ -5,18 +5,24 @@ import numpy as np
 
 
 def coolerProjections():
-    m = Basemap(projection='mill',llcrnrlat=47.1,urcrnrlat=55.2, llcrnrlon=5.5,urcrnrlon=15.3, resolution='l')
+    #m = Basemap(projection='mill',llcrnrlat=47.1,urcrnrlat=55.2, llcrnrlon=5.5,urcrnrlon=15.3, resolution='l')
+    m = Basemap(width=12000000,height=9000000,projection='lcc', resolution='l',lat_1=5,lat_2=6,lat_0=52,lon_0=9)
 
-    m.drawcountries()
-    m.drawstates()
+    #m.drawcountries(linewidth=1.5)
+    #m.drawcoastlines(linewidth=1.5)
+    #m.drawstates()
 
 
-    m.bluemarble()
+    #m.bluemarble()
+
+    m.etopo()
 
     plt.title("Geo Plotting Tutorial")
     plt.show()
 
     #plt.savefig('map.png')
+
+# https://matplotlib.org/gallery/animation/dynamic_image2.html
 
 
 # are you getting an error like:
