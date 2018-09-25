63be02b41e439e6302af481adff2e7acbfef909c
Stefan Maibücher <s.maibuecher@gmail.com>
2018-09-25 17:13:44 +0200

    sql abfrage

--- map_plotter.py
+++ map_plotter.py
@@ -1,7 +1,7 @@
 from mpl_toolkits.basemap import Basemap
 import matplotlib.pyplot as plt
 import numpy as np
-
+import sqlite3
 
 
 def coolerProjections():
@@ -17,12 +17,13 @@ def coolerProjections():
 
     m.etopo()
 
-    plt.title("Geo Plotting Tutorial")
+    plt.title("GCF")
     plt.show()
 
     #plt.savefig('map.png')
 
-# https://matplotlib.org/gallery/animation/dynamic_image2.html
+
+#https://stackoverflow.com/questions/753190/programmatically-generate-video-or-animated-gif-in-python
 
 
 # are you getting an error like:
@@ -31,4 +32,15 @@ def coolerProjections():
 # If it says you don't have PIL at all, then you better go grab it
 # but this should have come with your matplotlib installation.
 
-coolerProjections()
+
+def loadSqlEntries():
+	connection = sqlite3.connect("first_valid_db.sqlite")
+	cursor = connection.cursor()
+
+	cursor.execute("SELECT * FROM CityTable LIMIT 5;") 
+	print("\nfetch one:")
+	res = cursor.fetchall() #cursor.fetchone() 
+	print(res)
+
+#coolerProjections()
+loadSqlEntries()
